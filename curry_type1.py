091ed62c440daf4d75e8e2d07f72b3c5d0728fe7
ryuuichi0731 <39109695+ryuuichi0731@users.noreply.github.com>
2020-04-13 17:02:59 +0900

    Add files via upload

---
+++ curry_type1.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+import sys
+sys.path.append('./vendor')
+
+import os
+import uuid
+
+from PIL import Image
+import io
+
+from flask import Flask, request, abort, send_file
+
+from linebot import (
+    LineBotApi, WebhookHandler,
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, MessageImagemapAction, ImagemapArea, ImagemapSendMessage, BaseSize
+)
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi(os.environ.get('CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.environ.get('CHANNEL_SECRET'))
+
+
+@app.route("/", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    if event.type == "message":
+        if event.message.type == "text":
+            actions = []
+            actions.append(MessageImagemapAction(
+                  text = 'chicken_curry',
+                  area = ImagemapArea(
+                      x = 29, y = 60, width = 314, height = 338
+                  )
+            ))
+            actions.append(MessageImagemapAction(
+                  text = 'green_curry',
+                  area = ImagemapArea(
+                      x = 359, y = 61, width = 316, height = 340
+                  )
+            ))
+            actions.append(MessageImagemapAction(
+                  text = 'keema_curry',
+                  area = ImagemapArea(
+                      x = 695, y = 58, width = 317, height = 344
+                  )
+            ))
+
+            message = ImagemapSendMessage(
+                base_url = 'https://' + request.host + '/imagemap/' + uuid.uuid4().hex, # prevent cache
+                alt_text = '代替テキスト',
+                base_size = BaseSize(height=460, width=1040),
+                actions = actions
+            )
+            line_bot_api.reply_message(event.reply_token, message)
+
+@app.route("/imagemap//", methods=['GET'])
+def imagemap(uniqid, size):
+    img = Image.open("./imagemap.png")
+    img_resize = img.resize((int(size), int(size)))
+    img_io = io.BytesIO()
+    img_resize.save(img_io, 'PNG')
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/png')
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
