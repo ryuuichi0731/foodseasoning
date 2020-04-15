559fbfd894f3eb4567c0698e69f666ee864f19d6
ryuuichi0731 <39109695+ryuuichi0731@users.noreply.github.com>
2020-04-15 13:54:23 +0900

    Update curry_type1.py

--- curry_type1.py
+++ curry_type1.py
@@ -1,48 +1,18 @@
-# -*- coding: utf-8 -*-
-import sys
-sys.path.append('./vendor')
+from __future__ import unicode_literals, absolute_import
 
-import os
-import uuid
+import json
+import unittest
 
-from PIL import Image
-import io
-
-from flask import Flask, request, abort, send_file
+import responses
 
 from linebot import (
-    LineBotApi, WebhookHandler,
-)
-from linebot.exceptions import (
-    InvalidSignatureError
+    LineBotApi
 )
 from linebot.models import (
-    MessageEvent, TextMessage, MessageImagemapAction, ImagemapArea, ImagemapSendMessage, BaseSize
+    ImagemapSendMessage, BaseSize, URIImagemapAction,
+    ImagemapArea, MessageImagemapAction,
+    Video, ExternalLink
 )
-
-app = Flask(__name__)
-
-line_bot_api = LineBotApi(os.environ.get('CHANNEL_ACCESS_TOKEN'))
-handler = WebhookHandler(os.environ.get('CHANNEL_SECRET'))
-
-
-@app.route("/", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
-
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
