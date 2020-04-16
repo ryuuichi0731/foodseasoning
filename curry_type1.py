090f4e5e37fbfe87f89716d5cec64ab41a5a6ffd
ryuuichi0731 <39109695+ryuuichi0731@users.noreply.github.com>
2020-04-16 15:06:38 +0900

    Update curry_type1.py

--- curry_type1.py
+++ curry_type1.py
@@ -1,170 +1,85 @@
-from __future__ import unicode_literals, absolute_import
-
-import json
-import unittest
-
-import responses
+from flask import Flask, request, abort
 
 from linebot import (
-    LineBotApi
+    LineBotApi, WebhookHandler
+)
+
+from linebot.exceptions import (
+    InvalidSignatureError
 )
 from linebot.models import (
-    ImagemapSendMessage, BaseSize, URIImagemapAction,
-    ImagemapArea, MessageImagemapAction, ExternalLink
+    ImagemapArea,
+    ImagemapSendMessage,
+    URIImagemapAction,
+    MessageImagemapAction,
+    Video, BaseSize, ExternalLink,
 )
 
+import os
+import json
 
-class TestLineBotApi(unittest.TestCase):
-    def setUp(self):
-        self.tested = LineBotApi('channel_secret')
-        self.imagemap_message = ImagemapSendMessage(
-            base_url='https://baseurl.com/image/ct01',
-            alt_text='this is an imagemap',
-            base_size=BaseSize(height=453, width=1040),
+#アクセスキーの取得
+app = Flask(__name__)
+
+#環境変数取得
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+#ここからーーー ーーー 
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    if event.type == “message”:
+        if event.message.type == “テスト”:
             
-            @handler.add(MessageEvent, message=TextMessage)
-            def handle_message(event):
-            if event.type == "message":
-                if event.message.type == "text":
-            
+            imagemap_message = ImagemapSendMessage(
+                base_url='https://baseurl.com/image/ct01',
+                alt_text='this is an imagemap',
+                
+                base_size=BaseSize(height=453, width=1040),
                 actions=[
                     MessageImagemapAction(
                         text='hello',
                         area=ImagemapArea(
-                            x=40, y=56, width=313, height=340
+                            x=35, y=60, width=310, height=335
                         )
                     ),
                     MessageImagemapAction(
                         text='hello',
                         area=ImagemapArea(
-                            x=363, y=59, width=314, height=337
+                            x=365, y=60, width=310, height=335
                         )
                     ),
                     MessageImagemapAction(
                         text='hello',
                         area=ImagemapArea(
-                            x=690, y=60, width=317, height=335
+                            x=690, y=60, width=310, height=335
                         )
                     )
                 ]
             )
+            line_bot_api.push_message(event.source.user_id, imagemap_message)
 
-            self.message = [{
-                "type": "imagemap",
-                "baseUrl": "https://example.com/base",
-                "altText": "this is an imagemap",
-                "baseSize": {
-                    "height": 453,
-                    "width": 1040
-                },
-            
-                "actions": [
-                    {
-                        "type": "uri",
-                        "text": "hello",
-                        "area": {
-                            "x": 40,
-                            "y": 56,
-                            "width": 313,
-                            "height": 340
-                        }
-                    },
-                    {
-                        "type": "message",
-                        "text": "hello",
-                        "area": {
-                            "x": 363,
-                            "y": 59,
-                            "width": 314,
-                            "height": 337
-                        }
-                    },
-                    {
-                        "type": "message",
-                        "text": "hello",
-                        "area": {
-                            "x": 690,
-                            "y": 60,
-                            "width": 317,
-                            "height": 335
-                        }
-                    }
-                ]
-            }]
-            line_bot_api.reply_message(event.reply_token, message)
-
-        @responses.activate
-        def test_push_imagemap_message(self):
-            responses.add(
-                responses.POST,
-                LineBotApi.DEFAULT_API_ENDPOINT + '/v2/bot/message/push',
-                json={}, status=200
-            )
-
-            self.tested.push_message('to', self.imagemap_message)
-
-            request = responses.calls[0].request
-            self.assertEqual(request.method, 'POST')
-            self.assertEqual(
-                request.url,
-                LineBotApi.DEFAULT_API_ENDPOINT + '/v2/bot/message/push')
-            self.assertEqual(
-                json.loads(request.body),
-                {
-                    "to": "to",
-                    'notificationDisabled': False,
-                    "messages": self.message
-                }
-            )
-
-        @responses.activate
-        def test_reply_imagemap_message(self):
-            responses.add(
-                responses.POST,
-                LineBotApi.DEFAULT_API_ENDPOINT + '/v2/bot/message/reply',
-                json={}, status=200
-            )
-
-            self.tested.reply_message('replyToken', self.imagemap_message)
-
-            request = responses.calls[0].request
-            self.assertEqual(request.method, 'POST')
-            self.assertEqual(
-                request.url,
-                LineBotApi.DEFAULT_API_ENDPOINT + '/v2/bot/message/reply')
-            self.assertEqual(
-                json.loads(request.body),
-                {
-                    "replyToken": "replyToken",
-                    'notificationDisabled': False,
-                    "messages": self.message
-                }
-            )
-
-        @responses.activate
-        def test_multicast_imagemap_message(self):
-            responses.add(
-                responses.POST,
-                LineBotApi.DEFAULT_API_ENDPOINT + '/v2/bot/message/multicast',
-                json={}, status=200
-            )
-
-            self.tested.multicast(['to1', 'to2'], self.imagemap_message)
-
-            request = responses.calls[0].request
-            self.assertEqual(request.method, 'POST')
-            self.assertEqual(
-                request.url,
-                LineBotApi.DEFAULT_API_ENDPOINT + '/v2/bot/message/multicast')
-            self.assertEqual(
-                json.loads(request.body),
-                {
-                    "to": ['to1', 'to2'],
-                    'notificationDisabled': False,
-                    "messages": self.message
-                }
-            )
-
-
-    if __name__ == '__main__':
-        unittest.main()
+if __name__ == '__main__':
+    unittest.main()
+   
