2b92f1de0850ac7e47f30ad7657993c3ea4bfbf7
ryuuichi0731 <39109695+ryuuichi0731@users.noreply.github.com>
2020-04-19 00:10:39 +0900

    Update curry_type1.py

--- curry_type1.py
+++ curry_type1.py
@@ -1,11 +1,49 @@
+from flask import Flask, request, abort
 
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+
+from linebot.exceptions import (
+    InvalidSignatureError
+)
 from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
     ImagemapArea,
     ImagemapSendMessage,
     URIImagemapAction,
     MessageImagemapAction,
     Video, BaseSize, ExternalLink,
 )
+import os
+import json
+
+#アクセスキーの取得
+app = Flask(__name__)
+
+#環境変数取得
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
 
 #ここからーーー ーーー 
 
@@ -41,3 +79,27 @@ def handle_message(event):
                 ]
             )
             line_bot_api.push_message(event.source.user_id, imagemap_message)
+            
+            
+            
+    message_content = line_bot_api.get_message_content(event.message.id)
+    with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix=ext + '-', delete=False) as tf:
+        for chunk in message_content.iter_content():
+            tf.write(chunk)
+        tempfile_path = tf.name
+
+    dist_path = tempfile_path + '.' + ext
+    dist_name = os.path.basename(dist_path)
+    os.rename(tempfile_path, dist_path)
+
+    line_bot_api.reply_message(
+        event.reply_token, [
+            TextSendMessage(text='Save content.'),
+            TextSendMessage(text=request.host_url + os.path.join('static', 'tmp', dist_name))
+        ])
+
+
+if __name__ == "__main__":
+#    app.run()
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
