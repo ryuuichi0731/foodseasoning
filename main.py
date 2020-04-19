efbe4049731c8a37f8d061185b04a9d63fdfef67
ryuuichi0731 <39109695+ryuuichi0731@users.noreply.github.com>
2020-04-19 13:06:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,43 @@
-# -*- coding: utf-8 -*-
-import sys
-sys.path.append('./vendor')
-
-import os
-import uuid
-
-from PIL import Image
-import io
-
-from flask import Flask, request, abort, send_file
+from flask import Flask, request, abort
 
 from linebot import (
-    LineBotApi, WebhookHandler,
+    LineBotApi, WebhookHandler
 )
+
 from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, MessageImagemapAction, ImagemapArea, ImagemapSendMessage, BaseSize
+    MessageEvent, TextMessage, TextSendMessage,
+    SourceUser, SourceGroup, SourceRoom,
+    TemplateSendMessage, ConfirmTemplate, MessageAction,
+    ButtonsTemplate, ImageCarouselTemplate, ImageCarouselColumn, URIAction,
+    PostbackAction, DatetimePickerAction,
+    CameraAction, CameraRollAction, LocationAction,
+    CarouselTemplate, CarouselColumn, PostbackEvent,
+    StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
+    ImageMessage, VideoMessage, AudioMessage, FileMessage,
+    UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent,
+    FlexSendMessage, BubbleContainer, ImageComponent, BoxComponent,
+    TextComponent, SpacerComponent, IconComponent, ButtonComponent,
+    SeparatorComponent, QuickReply, QuickReplyButton, 
+    ImagemapArea, ImagemapSendMessage, URIImagemapAction, MessageImagemapAction, Video, BaseSize, ExternalLink,
 )
 
+import os
+import json
+
+#アクセスキーの取得
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ.get('i902NfQKjb+No9Cmmrd+n5Zyrq4jZ8msx1RuG80KN9cYddzt/fgcSsB5B5Bj78+43MLiZOAd7zAAzz381t83Lefhjpk5V9sGIGZJzmOhgK51Rr/rNv9h7AZ911Pn0RLdjsNBjCSdycUMsFznpKcgwAdB04t89/1O/w1cDnyilFU='))
-handler = WebhookHandler(os.environ.get('5600f21c984f16ef30586eef1e3d86ee'))
+#環境変数取得
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
-@app.route("/", methods=['POST'])
+@app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
@@ -43,48 +54,63 @@ def callback():
 
     return 'OK'
 
+#ここから------
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if event.type == "message":
-        if event.message.type == "text":
-            actions = []
-            actions.append(MessageImagemapAction(
-                  text = '１',
-                  area = ImagemapArea(
-                      x = 31, y = 49, width = 322, height = 355
-                  )
-            ))
-            actions.append(MessageImagemapAction(
-                  text = '２',
-                  area = ImagemapArea(
-                      x = 360, y = 49, width = 324, height = 365
-                  )
-            ))
-            actions.append(MessageImagemapAction(
-                  text = '３',
-                  area = ImagemapArea(
-                      x = 689, y = 42, width = 316, height = 368
-                  )
-            ))
-            
-            message = ImagemapSendMessage(
-                base_url = 'https://foodseasoning.web.fc2.com/img/images01' + request.host + '/imagemap/' + uuid.uuid4().hex, # prevent cache
-                alt_text = '代替テキスト',
-                base_size = BaseSize(height=453, width=1040),
-                actions = actions
+        if (event.message.text == "カレーメニュー"):
+            imagemap_message = ImagemapSendMessage(
+  
+                base_url='https://foodseasoning.web.fc2.com/img/images01',
+  
+                alt_text='this is an imagemap',
+  
+                base_size=BaseSize(height=453, width=1040),
+  
+                actions=[
+                    MessageImagemapAction(
+                        text='hello',
+                        area=ImagemapArea(
+                            x=30, y=50, width=320, height=355
+                        )
+                    ),
+                    MessageImagemapAction(
+                        text='hello',
+                        area=ImagemapArea(
+                            x=360, y=50, width=320, height=355
+                        )
+                    ),
+                    MessageImagemapAction(
+                        text='hello',
+                        area=ImagemapArea(
+                            x=690, y=50, width=320, height=355
+                        )
+                    )
+                ]
             )
-            line_bot_api.reply_message(event.reply_token, message)
+            line_bot_api.push_message(event.source.user_id, imagemap_message)
 
-@app.route("/imagemap//", methods=['GET'])
-def imagemap(uniqid, size):
-    img = Image.open("./imagemap.png")
-    img_resize = img.resize((int(size), int(size)))
-    img_io = io.BytesIO()
-    img_resize.save(img_io, 'PNG')
-    img_io.seek(0)
-    return send_file(img_io, mimetype='image/png')
+#ここまで------                  
+                
+    message_content = line_bot_api.get_message_content(event.message.id)
+    with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix=ext + '-', delete=False) as tf:
+        for chunk in message_content.iter_content():
+            tf.write(chunk)
+        tempfile_path = tf.name
+
+    dist_path = tempfile_path + '.' + ext
+    dist_name = os.path.basename(dist_path)
+    os.rename(tempfile_path, dist_path)
+
+    line_bot_api.reply_message(
+        event.reply_token, [
+            TextSendMessage(text='Save content.'),
+            TextSendMessage(text=request.host_url + os.path.join('static', 'tmp', dist_name))
+        ])
+
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+#    app.run()
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
