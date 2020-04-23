4997b1e52e65a0a9b08b11d68d8cc9e503e568c2
ryuuichi0731 <39109695+ryuuichi0731@users.noreply.github.com>
2020-04-23 13:48:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -95,9 +95,61 @@ def handle_message(event):
             )
             line_bot_api.push_message(event.source.user_id, imagemap_message)
             
-    
-    
-#ここまで------                  
+    if event.type == "message":
+        if (event.message.text == "chicken_curry"):
+            
+            bubble = BubbleContainer(
+            direction='ltr',
+            hero=ImageComponent(
+                url='https://images.unsplash.com/photo-1585937421612-70a008356fbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+                size='full',
+                aspect_ratio='20:13',
+                aspect_mode='cover',
+                
+            body=BoxComponent(
+                layout='vertical',
+                contents=[
+                    # title
+                    TextComponent(text='チキンカレー', weight='bold', size='xl'),
+                     # material
+                    BoxComponent(
+                                layout='baseline',
+                                spacing='sm',
+                                contents=[
+                                    TextComponent(
+                                        text='材料\n　・鶏モモ肉\n　・タマネギ\n　・シメジ\n　・トマトソース（お好きな調味料）\n　・水\n　・オリーブオイル\n　・バター\n　・カレー（お好きなスパイス、ハーブ）',
+                                        color='#aaaaaa',
+                                        size='sm',
+                                        flex=1
+                                    ),
+                footer=BoxComponent(
+                    layout='vertical',
+                    spacing='sm',
+                    contents=[
+                        SpacerComponent(size='sm'),
+                        ButtonComponent(
+                            style='text',
+                            height='sm',
+                            action=MessageAction(label='選びなおす', text='cancel_chicken_curry'),
+                        ),
+                        SeparatorComponent(),
+                        ButtonComponent(
+                            style='text',
+                            height='sm',
+                            action=MessageAction(label='次へ進む', text="next_chicken_curry")
+                        )
+                    ]
+                ),
+                                    
+        )
+        message = FlexSendMessage(alt_text="curry_menu_CC", contents=bubble)
+        line_bot_api.reply_message(
+            event.reply_token,
+            message
+        )
+           
+#ここまで------ 
+
                 
     message_content = line_bot_api.get_message_content(event.message.id)
     with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix=ext + '-', delete=False) as tf:
