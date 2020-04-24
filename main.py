b581e7eca42a174b3e4c4a788f29c6d0fbbfdfc3
ryuuichi0731 <39109695+ryuuichi0731@users.noreply.github.com>
2020-04-24 16:14:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -127,7 +127,7 @@ def handle_message(event):
                                             flex=1
                                         ),
                                         TextComponent(
-                                            text='①鶏モモ肉\n ②タマネギ\n ③シメジ\n ④水\n ⑤オリーブオイル\n ⑥バター',
+                                            text=' ①鶏モモ肉\n ②タマネギ\n ③シメジ\n ④水\n ⑤オリーブオイル\n ⑥バター',
                                             wrap=True,
                                             color='#666666',
                                             size='sm',
@@ -146,7 +146,7 @@ def handle_message(event):
                                             flex=1
                                         ),
                                         TextComponent(
-                                            text=" ⑦トマトソース（お好きな調味料）\n ⑧お好きなスパイス",
+                                            text=" ⑦お好きな調味料\n　（トマトソース）　（＋）\n ⑧お好きなスパイス",
                                             wrap=True,
                                             color='#666666',
                                             size='sm',
